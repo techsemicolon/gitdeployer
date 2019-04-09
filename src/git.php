bae4dbd6b7a44911cbd9fd825ffb369ae6be0a0d
techsemicolon <tech.semicolon@gmail.com>
2019-04-09 12:50:49 +0530

    Adding comments for config file

--- src/git.php
+++ src/git.php
@@ -4,29 +4,55 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Cache Store
+    | Git dir absolute path
     |--------------------------------------------------------------------------
     |
-    | This option controls the default cache connection that gets used while
-    | using this caching library. This connection is used when another is
-    | not explicitly specified when executing a given caching function.
-    |
-    | Supported: "apc", "array", "database", "file", "memcached", "redis"
+    | Specify full absolute path of git dir
+    | This will help if your .git folder is not inside the working directory
+    | The value of this will be passed like `git pull --git-dir=/your/path/repo.git`
+    | This is optional
     |
     */
 
     'dir' => env('GIT_DIR', null),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Before script name
+    |--------------------------------------------------------------------------
+    |
+    | This the name of script to run before running main deployment script
+    | The script files should be inside webhookscripts folder
+    | This is optional
+    |
+    */
+
     'before_script' => env('GIT_BEFORE_SCRIPT', null),
     
+    /*
+    |--------------------------------------------------------------------------
+    | After script name
+    |--------------------------------------------------------------------------
+    |
+    | This the name of script to run after running main deployment script
+    | The script files should be inside webhookscripts folder
+    | This is optional
+    |
+    */
     'after_script' => env('GIT_AFTER_SCRIPT', null),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Bitbucket official IP ranges
+    |--------------------------------------------------------------------------
+    |
+    | These are the official bitbucket IP addresses which needs to be whitelisted
+    | This is required
+    |
+    */
     'bitbucket_ips' => [
         '18.205.93.0/25',
         '18.234.32.128/25',
         '13.52.5.0/25'
     ],
-
-    
-
 ];
